a3c528eeaedff587ccdf1fff5043e50b407062ca
hanzala211 <hanzalaoc211@gmail.com>
2025-04-09 12:37:02 +0500

    fixed structure issues

--- src/pages/Messages/components/MessagesLeftPanel.tsx
+++ src/pages/Messages/components/MessagesLeftPanel.tsx
@@ -2,7 +2,6 @@ import { Message } from './Message';
 import { CheckInput } from '@components';
 import { useMessage } from '@context';
 import { useEffect, useState } from 'react';
-import { ICONS } from '@constants';
 import { useQuery } from '@tanstack/react-query';
 import { IMessage } from '@types';
 import MessageSkeleton from './MessageSkeleton';
@@ -11,11 +10,13 @@ import { useNavigate } from 'react-router-dom';
 export const MessagesLeftPanel: React.FC = () => {
   const { setSelectedMessage, selectedMessage, getMessages } = useMessage();
   const [messagesBoolean, setMessagesBoolean] = useState<boolean[]>([]);
-  const { data: messages, isLoading: isMessagesLoading } = useQuery<IMessage[] | undefined>({
-    queryKey: ["messages"],
-    queryFn: getMessages
-  })
-  const navigate = useNavigate()
+  const { data: messages, isLoading: isMessagesLoading } = useQuery<
+    IMessage[] | undefined
+  >({
+    queryKey: ['messages'],
+    queryFn: getMessages,
+  });
+  const navigate = useNavigate();
 
   const selectAll = messagesBoolean.every((item) => item === true);
 
@@ -42,15 +43,26 @@ export const MessagesLeftPanel: React.FC = () => {
     setMessagesBoolean(updatedMessages);
   };
 
+  const filters = [
+    { label: 'Recent', value: 'recent' },
+    { label: 'Oldest', value: 'oldest' },
+  ];
+
   return (
     <div
       className={`md:block w-full h-full ${selectedMessage === null ? 'block' : 'hidden'
         }`}
     >
       <div className="flex justify-end w-full">
-        <span className="px-4 py-2 flex gap-2 items-center cursor-pointer">
-          Newest <ICONS.arrowDown />
-        </span>
+        <select
+          className="w-fit mr-2 mt-3 rounded-md focus:outline-none focus:border-darkBlue hover:border-darkBlue transition-all duration-200 bg-white py-[3px] px-3 focus-within:outline-none border-basicBlack border-[1px]"
+        >
+          {filters.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
+        </select>
       </div>
       <div className="md:px-10 px-2 mt-4 w-full">
         <div className="flex gap-2 border-b-[1px] border-silverGray pb-3 mb-3">
@@ -59,22 +71,28 @@ export const MessagesLeftPanel: React.FC = () => {
             handleOnChange={handleSelectAll}
             label="select"
           />
-          <label htmlFor="select" className="font-semibold">
+          <label htmlFor="select" className="font-semibold m-0">
             Select All
           </label>
         </div>
         <div className="flex flex-col gap-3 h-[100dvh] max-h-[calc(100dvh-300px)] overflow-y-auto mt-2 w-full">
-          {!isMessagesLoading ? messages && messages.length > 0 && messages.map((item, index) => (
-            <Message
-              key={index}
-              onCheckChange={(value) => handleIndividualChange(index, value)}
-              index={index}
-              item={item}
-              messagesBoolean={messagesBoolean}
-            />
-          )) : <div>
-            <MessageSkeleton />
-          </div>}
+          {!isMessagesLoading ? (
+            messages &&
+            messages.length > 0 &&
+            messages.map((item, index) => (
+              <Message
+                key={index}
+                onCheckChange={(value) => handleIndividualChange(index, value)}
+                index={index}
+                item={item}
+                messagesBoolean={messagesBoolean}
+              />
+            ))
+          ) : (
+            <div>
+              <MessageSkeleton />
+            </div>
+          )}
         </div>
       </div>
     </div>
